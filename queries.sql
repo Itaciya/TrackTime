17695e1dc28ee66dcfb433b449bd611ca4a734b2
Itaciya <psfq606@gmail.com>
2025-10-08 08:12:17 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,4 +6,12 @@ CREATE TABLE Passengers (
   email varchar(100) NOT NULL,
   num char(15)
 );
-
+CREATE TABLE Alerts (
+    alert_id INT PRIMARY KEY,
+    passenger_id INT,
+    train_id INT,
+    alert_message VARCHAR(255),
+    alert_time DATETIME,
+    alert_type VARCHAR(30),
+    FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id)
+);
