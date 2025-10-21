37e98c58b4e771050dabb3186b28f5948d040f6c
Farhana Emrose Bintee <farhanabintee.29@gmail.com>
2025-10-21 23:36:40 +0600

    Update allqueries.sql

--- allqueries.sql
+++ allqueries.sql
@@ -1,11 +1,13 @@
-SELECT route_id, total_distance_km, estimated_time_minutes
-FROM Routes
-WHERE total_distance_km BETWEEN 20 AND 30
-ORDER BY estimated_time_minutes ASC;
+SELECT * FROM Routes;
 
 SELECT DISTINCT pass_type
 FROM MetroPass
 LIMIT 2;
+
+SELECT route_id, total_distance_km, estimated_time_minutes
+FROM Routes
+WHERE total_distance_km BETWEEN 20 AND 30
+ORDER BY estimated_time_minutes ASC;
 
 SELECT full_name, pass_type, balance
 FROM MetroPass
@@ -16,11 +18,3 @@ SELECT s.schedule_id, d.delay_minutes, d.reason
 FROM Schedules s
 JOIN Delays d ON s.schedule_id = d.schedule_id
 WHERE d.delay_minutes BETWEEN 5 AND 10;
-
-SELECT full_name, pass_type, balance
-FROM MetroPass
-WHERE balance > (
-    SELECT AVG(balance)
-    FROM MetroPass
-)
-ORDER BY balance DESC;
